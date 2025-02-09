b4bd3f08a620495797d39cae903c1a5263e532ba
Oscar Chen <oscar.y.chen@gmail.com>
2025-02-09 12:49:39 -0700

    add tags to locust tasks

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -1,45 +1,46 @@
 
 import threading
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, FastHttpUser, tag
 
 # Create a global lock object
 task_lock = threading.Lock()
 
-class ApiLoadTest(HttpUser):
+class ApiLoadTest(FastHttpUser):
     wait_time = between(1, 3)  # Time between tasks (1 to 3 seconds)
     host = "http://localhost"
+
+    @tag("drf_with_serializer")
     @task(1)
     def test_drf_with_serializer(self):
         with task_lock:  # Only one task can run at a time due to this lock
-            response = self.client.get("http://localhost:8000/drf/with-serializer/")
-            print(f"DRF with serializer response time: {response.elapsed.total_seconds()} seconds")
+            self.client.get("http://localhost:8000/drf/with-serializer/")
 
+    @tag("drf_without_serializer")
     @task(1)
     def test_drf_without_serializer(self):
         with task_lock:
-            response = self.client.get("http://localhost:8000/drf/without-serializer/")
-            print(f"DRF without serializer response time: {response.elapsed.total_seconds()} seconds")
+            self.client.get("http://localhost:8000/drf/without-serializer/")
 
+    @tag("ninja_with_schema")
     @task(1)
     def test_ninja_with_schema(self):
         with task_lock:
-            response = self.client.get("http://localhost:8001/ninja/with-schema/")
-            print(f"Django ninja with schema response time: {response.elapsed.total_seconds()} seconds")
+            self.client.get("http://localhost:8001/ninja/with-schema/")
 
+    @tag("ninja_without_schema")
     @task(1)
     def test_ninja_without_schema(self):
-        # with task_lock:
-        response = self.client.get("http://localhost:8001/ninja/without-schema/")
-        print(f"Django ninja without schema response time: {response.elapsed.total_seconds()} seconds")
+        with task_lock:
+            self.client.get("http://localhost:8001/ninja/without-schema/")
 
+    @tag("fastapi_with_pydantic")
     @task(1)
     def test_fastapi_with_pydantic(self):
         with task_lock:
-            response = self.client.get("http://localhost:8002/fastapi/")
-            print(f"FastAPI response time: {response.elapsed.total_seconds()} seconds")
+            self.client.get("http://localhost:8002/fastapi/")
 
+    @tag("go")
     @task(1)
     def test_go(self):
         with task_lock:
-            response = self.client.get("http://localhost:8003/go/")
-            print(f"Go response time: {response.elapsed.total_seconds()} seconds")
+            self.client.get("http://localhost:8003/go/")
